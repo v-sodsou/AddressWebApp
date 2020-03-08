8487a529fc1492bcbdc1da45b976a7ea394c6653
v-sodsou <sonali.18feb@gmail.com>
2020-03-08 15:39:38 -0700

    added address service

---
+++ src/app/address.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddressService {
+
+  constructor(private http: HttpClient) { }
+
+  getCountries(){
+
+  }
+
+  getCountryFormat(countryCode){
+    
+    }
+
+  postAddress(){
+    
+  }
+
+  postAddressFormat(){
+    
+  }
+
+  getAddress(){
+
+  }
+
+}
